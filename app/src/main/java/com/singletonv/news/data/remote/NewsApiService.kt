e9bf9603b39a4770d1bf5c54f89c16592f6049ce
SingletonVD <singletonvd@mail.ru>
2025-10-21 05:24:07 +0300

    Move api key to ignored file, revoke compromised api key

--- app/src/main/java/com/singletonv/news/data/remote/NewsApiService.kt
+++ app/src/main/java/com/singletonv/news/data/remote/NewsApiService.kt
@@ -1,11 +1,12 @@
 package com.singletonv.news.data.remote
 
+import com.singletonv.news.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApiService {
 
-    @GET("v2/everything?apiKey=3a211640eddb4671952f7b9fd1fcb90f")
+    @GET("v2/everything?apiKey=${BuildConfig.NEWS_API_KEY}")
     suspend fun loadArticles(
         @Query("q") topic: String,
         @Query("language") language: String
